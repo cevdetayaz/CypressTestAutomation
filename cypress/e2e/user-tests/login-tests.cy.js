a47edf292471060d61fc2fd4689eeb19defed3bc
OmersGitHup <omerfaruk.isiq@gmail.com>
2024-07-22 21:42:05 +0300

    Tested Login and Login Unsuccsessful

--- cypress/e2e/user-tests/login-tests.cy.js
+++ cypress/e2e/user-tests/login-tests.cy.js
@@ -2,8 +2,21 @@
 
 describe('Login Test', () => {
 
-    it('Login Succsessful', () => {
-       cy.visit("https://www.netflix.com/login");
-      })
+    it.only('Login Succsessful', () => {
+        cy.visit("https://www.netflix.com/login");
+        cy.get("[name='userLoginId']").type("omerfaruk.isiq@gmail.com");
+        cy.get("[data-uia='password-field']").type("KadıköyIstanbul");
+        cy.get("[data-uia='login-submit-button']").click();
+        cy.wait(3000);
+        cy.get("[id='signOut']").should('have.text', "Oturumu Kapat");
+    })
+
+    it('Login Unsuccsessful', () => {
+        cy.visit("https://www.netflix.com/login");
+        cy.get("[name='userLoginId']").type("omerfaruk@gmail.com");
+        cy.get("[data-uia='password-field']").type("KadıköyIstanbul");
+        cy.get("[data-uia='login-submit-button']").click();
+        cy.get("[data-uia='error-message-container+header']").should('have.text', "Incorrect password for omerfaruk@gmail.com");
+    })
 
 })
