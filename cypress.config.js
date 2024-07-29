ff71ffe894114ca16c9751ca281c96449ac25b4a
Cevdet Ayaz <cevdetayaz@gmail.com>
2024-07-29 20:49:33 +0300

    cypress cloud için project id eklendi

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
+module.exports = defineConfig({  
   e2e: {
+    projectId: "mu8c8d",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
